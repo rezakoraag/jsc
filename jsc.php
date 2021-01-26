0049e41a82cd261f98d49d54cec1cc271ad3dd7d
rezakoraag <78005123+rezakoraag@users.noreply.github.com>
2021-01-26 17:14:35 +0700

    Update jsc.php

--- jsc.php
+++ jsc.php
@@ -34,5 +34,63 @@ class Jsc extends CI_Controller {
 
 		$resultKelurahan = json_decode($output2);
 	    curl_close($ch);
+
+
+		foreach($resultRS->data as $key=>$val)
+	    {
+
+			foreach($resultKelurahan->data as $key2=>$val2)
+			 {
+				 if($val->kode_kelurahan == $val2->kode_kelurahan){
+					 $nama_kel = $val2->nama_kelurahan;
+					 break;
+				 };
+			 }
+			 foreach($resultKelurahan->data as $key2=>$val2)
+ 			 {
+ 				 if($val->kode_kecamatan == $val2->kode_kecamatan){
+ 					 $nama_kec = $val2->nama_kecamatan;
+ 					 break;
+ 				 };
+ 			 }
+			 foreach($resultKelurahan->data as $key2=>$val2)
+ 			 {
+ 				 if($val->kode_kota == $val2->kode_kota){
+ 					 $nama_kotanya = $val2->nama_kota;
+ 					 break;
+ 				 };
+ 			 }
+
+			$config[] = [
+
+					   'nama_rsu' => $val->nama_rsu,
+					   'jenis_rsu' => $val->jenis_rsu,
+					   'location' => [
+						   'latitude' => $val->location->latitude,
+						   'longitude' => $val->location->longitude,
+					   ],
+					   'alamat' => $val->location->alamat,
+					   'kode_pos' => $val->kode_pos,
+					   'telepon' => $val->telepon,
+					   'faximile' => $val->faximile,
+					   'website' => $val->website,
+					   'email' => $val->email,
+					   'kelurahan' => [
+						   'kode' => $val->kode_kelurahan,
+						   'nama' => $nama_kel,
+					   ],
+					   'kecamatan' => [
+						   'kode' => $val->kode_kecamatan,
+						   'nama' => $nama_kec,
+					   ],
+					   'kota' => [
+						   'kode' => $val->kode_kota,
+						   'nama' => $nama_kotanya,
+					   ],
+			   ];
+	    }
+
+		var_dump($config);
+
 	}
 }
